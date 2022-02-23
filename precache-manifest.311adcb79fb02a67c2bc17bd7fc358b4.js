9f4ae0e37c308099f3ad9e4d5219f7eae4294739
Nathan Vasquez <nathanvasquezdo1@gmail.com>
2022-02-22 20:45:07 -0800

    Updates

---
+++ precache-manifest.311adcb79fb02a67c2bc17bd7fc358b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5ae001e35408bc69ac7985c07fbdf86",
+    "url": "/TPF/index.html"
+  },
+  {
+    "revision": "55c2d21bb31ce91b2837",
+    "url": "/TPF/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "24adfc8ff4001b22d8cd",
+    "url": "/TPF/static/js/2.550c4537.chunk.js"
+  },
+  {
+    "revision": "55c2d21bb31ce91b2837",
+    "url": "/TPF/static/js/main.6a1d3031.chunk.js"
+  },
+  {
+    "revision": "faef66f997d3ea32623a",
+    "url": "/TPF/static/js/runtime-main.5b2c02de.js"
+  }
+]);
